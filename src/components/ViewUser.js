293c42171c34db020ad2a2116e88cc9b27ac1f8e
Avaneesh Singh <Avaneesh.Singh@Geminisolutions.com>
2024-04-22 10:52:16 +0530

    -- final changes --

--- src/components/ViewUser.js
+++ src/components/ViewUser.js
@@ -11,22 +11,22 @@ const ViewUser = () => {
   return (
     <div className="container">
       <h1>View All Users</h1>
-      <div className='row mt-3'>
-      <h3 className="col-2">Name</h3>
-          <h3 className="col-2">Email</h3>
-          <h3 className="col-2">Gender</h3>
-          <h3 className="col-2">Mobile</h3>
-          <h3 className="col-2">Category</h3>
-          <h3 className="col-2">Technology</h3>
+      <div className='row mt-3 gx-3'>
+      <h5 className="col-2">Name</h5>
+          <h5 className="col-3">Email</h5>
+          <h5 className="col-2">Gender</h5>
+          <h5 className="col-2">Mobile</h5>
+          <h5 className="col-1">Category</h5>
+          <h5 className="col-2">Technology</h5>
       </div>
       {data?.map((userData)=>(
         <div className="">
-        <div className="row">
+        <div className="row gx-3">
           <p className="col-2">{userData.name}</p>
-          <p className="col-2">{userData.email}</p>
+          <p className="col-3">{userData.email}</p>
           <p className="col-2">{userData.gender}</p>
           <p className="col-2">{userData.mobile}</p>
-          <p className="col-2">{userData.category}</p>
+          <p className="col-1">{userData.category}</p>
           <p className="col-2">{userData.technology.join(', ')}</p>
         </div>
       </div>
